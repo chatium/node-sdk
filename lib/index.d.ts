8e4b81531bee28a4a5a76411aa9d1eecf149a8cf
davojan <davojan@gmail.com>
2020-08-22 10:52:12 +0300

    0.0.2: exposed chatiumGet and chatiumPost

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 import type { AccountCtx, AppCtx, AuthCtx } from './context';
+export { chatiumGet, chatiumPost } from './api/chatiumApiClient';
 export { HeapRepo } from './heap/HeapRepo';
 export { AccountCtx, AppCtx, AuthCtx };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import type { AccountCtx, AppCtx, AuthCtx } from './context'
 
+export { chatiumGet, chatiumPost } from './api/chatiumApiClient'
 export { HeapRepo } from './heap/HeapRepo'
 export { AccountCtx, AppCtx, AuthCtx }
